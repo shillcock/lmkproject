0023252944d8f83f6d6feb338ffd65650dd81471
Scott Shillcock <scott@shillcock.net>
2008-06-03 15:28:40 -0700

    updated

--- global.lua
+++ global.lua
@@ -2,12 +2,9 @@
 lmk.set_global_env {
 
    DMZ_USE_QT=true,
-   DMZ_QT_HOME="$(lmk.projectRoot)/depend/bin",
+   DMZ_QT_HOME="$(lmk.projectRoot)depend/bin",
    DMZ_QT_INCLUDE_PATH="/Library/Frameworks",
    DMZ_QT_LIB_PATH="/Library/Frameworks",
---   DMZ_QT_HOME="/usr/local/Trolltech/Qt-4.4.0-tp1/bin",
---   DMZ_QT_INCLUDE_PATH="/usr/local/Trolltech/Qt-4.4.0-tp1/include",
---   DMZ_QT_LIB_PATH="/usr/local/Trolltech/Qt-4.4.0-tp1/lib",
    
    DMZ_USE_OGRE=true,
    DMZ_OGRE_INCLUDE_PATH="/Library/Frameworks",
@@ -18,6 +15,6 @@ lmk.set_global_env {
    DMZ_OSG_LIB_PATH="/Library/Frameworks",
    
    DMZ_USE_FMOD=true,
-   DMZ_FMOD_INCLUDE_PATH="$(lmk.projectRoot)/depend/fmod/api/inc",
-   DMZ_FMOD_LIB_PATH="$(lmk.projectRoot)/depend/fmod/api/lib",
+   DMZ_FMOD_INCLUDE_PATH="$(lmk.projectRoot)depend/fmod/api/inc",
+   DMZ_FMOD_LIB_PATH="$(lmk.projectRoot)depend/fmod/api/lib",
 }
