a305aea12309e4e87b25c2e0641a98d3c4497532
Scott Shillcock <scott@blade.(none)>
2008-10-10 15:57:56 -0700

    updated and added a script to help with getting setup

---
+++ dmzget.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+REPOS=(lmk dmz scripts cycles io mbra)
+SRC_DIR=/home/scott/cm/src
+GITHUB=git@github.com
+USERNAME=shillcock
+REMOTE=bluemarvin
+
+if [ -d $SRC_DIR ];
+then
+   echo "$SRC_DIR exists"
+else
+   echo "Creating $SRC_DIR"
+   mkdir -p $SRC_DIR
+fi
+
+cd $SRC_DIR
+
+for REPO in ${REPOS[@]}; do
+   git clone ${GITHUB}:${USERNAME}/${REPO}.git
+done
+
+for REPO in ${REPOS[@]}; do
+   cd ${SRC_DIR}/${REPO}
+   git remote add $REMOTE ${GITHUB}:${REMOTE}/${REPO}.git
+done
+
+
